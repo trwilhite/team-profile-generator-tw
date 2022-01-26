53ee34b5cbe36a99a41902d9070abd858d2517d7
Tamara Wilhite <wilhite.tamara6452@gmail.com>
2022-01-26 17:56:33 -0500

    wrote failing tests for intern class

---
+++ __tests__/Intern.test.js
@@ -0,0 +1,85 @@
+const Intern = require('../lib/Intern')
+
+describe('Intern', () => {
+    describe('intern object', () => {
+        it('should create a new employee object', () => {
+            const intern = new Intern('Jane', 1, 'janedoe@gmail.com');
+
+            expect(typeof(intern)).toBe('object');
+            expect(intern.name).toBe('Jane');
+            expect(intern.id).toEqual(1);
+            expect(intern.email).toBe('janedoe@gmail.com');
+        });
+    });
+
+    describe('intern name', () => {
+        it('should set name property', () => {
+            const name = 'Jane';
+            const intern = new Intern(name);
+            expect(intern.name).toBe(name);
+        });
+    });
+
+    describe('intern ID', () => {
+        it('should set employee ID property', () => {
+            const id = 1;
+            const intern = new Intern('Jane', id);
+            expect(intern.id).toBe(id)
+        });
+    });
+
+    describe('intern email', () => {
+        it('should set intern email property', () => {
+            const email = 'janedoe@gmail.com';
+            const intern = new Intern('Jane', 1, email);
+            expect(intern.email).toBe(email);
+        });
+    });
+
+    describe('intern school', () => {
+        it('should set intern school property', () => {
+            const school = 'university';
+            const intern = new Intern ('Jane', 1, 'janedoe@gmail.com', school);
+            expect(intern.school).toBe(school);
+        });
+    });
+
+    describe('get intern name', () => {
+        it('should retrieve intern name property', () => {
+            const name = 'Jane';
+            const intern = new Intern(name, 1, 'janedoe@gmail.com');
+            expect(intern.getName()).toBe(name);
+        });
+    });
+
+    describe('get intern id', () => {
+        it('should retrieve intern ID property', () => {
+            const id = 1;
+            const intern = new Intern('Jane', id, 'janedoe@gmail.com');
+            expect(intern.getId()).toBe(id);
+        });
+    });
+
+    describe('get intern email', () => {
+        it('should retrieve intern email', () => {
+            const email = 'janedoe@gmail.com';
+            const intern = new Intern('Jane', 1, email);
+            expect(intern.getEmail()).toBe(email);
+        });
+    });
+
+    describe('get intern school', () => {
+        it('should retrieve intern school', () => {
+            const school = 'university';
+            const intern = new Intern('Jane', 1, 'janedoe@gmail.com', school);
+            expect(intern.getSchool()).toBe(school);
+        });
+    });
+
+    describe('get intern role', () => {
+        it('should display intern as the role', () => {
+            const intern = new Intern();
+            expect(intern.getRole()).toBe('Intern');
+        });
+    });
+});
