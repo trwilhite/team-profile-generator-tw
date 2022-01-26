0a19c3a9cf11b07aa785abc64eb66234342d34b0
Tamara Wilhite <wilhite.tamara6452@gmail.com>
2022-01-26 17:47:58 -0500

    wrote failing tests for engineer class

---
+++ __tests__/Engineer.test.js
@@ -0,0 +1,85 @@
+const Engineer = require('../lib/Engineer')
+
+describe('Engineer', () => {
+    describe('engineer object', () => {
+        it('should create a new employee object', () => {
+            const engineer = new Engineer('Jane', 1, 'janedoe@gmail.com');
+
+            expect(typeof(engineer)).toBe('object');
+            expect(engineer.name).toBe('Jane');
+            expect(engineer.id).toEqual(1);
+            expect(engineer.email).toBe('janedoe@gmail.com');
+        });
+    });
+
+    describe('engineer name', () => {
+        it('should set name property', () => {
+            const name = 'Jane';
+            const engineer = new Engineer(name);
+            expect(engineer.name).toBe(name);
+        });
+    });
+
+    describe('engineer ID', () => {
+        it('should set engineer ID property', () => {
+            const id = 1;
+            const engineer = new Engineer('Jane', id);
+            expect(engineer.id).toBe(id)
+        });
+    });
+
+    describe('engineer email', () => {
+        it('should set engineer email property', () => {
+            const email = 'janedoe@gmail.com';
+            const engineer = new Engineer('Jane', 1, email);
+            expect(engineer.email).toBe(email);
+        });
+    });
+
+    describe('engineer github', () => {
+        it('should set engineer github property', () => {
+            const github = 'janedoe123';
+            const engineer = new Engineer('Jane', 1, 'janedoe@gmail.com', github);
+            expect(engineer.github).toBe(github);
+        });
+    });
+
+    describe('get engineer name', () => {
+        it('should retrieve engineer name property', () => {
+            const name = 'Jane';
+            const engineer = new Engineer(name, 1, 'janedoe@gmail.com');
+            expect(engineer.getName()).toBe(name);
+        });
+    });
+
+    describe('get engineer id', () => {
+        it('should retrieve engineer ID property', () => {
+            const id = 1;
+            const engineer = new Engineer('Jane', id, 'janedoe@gmail.com');
+            expect(engineer.getId()).toBe(id);
+        });
+    });
+
+    describe('get engineer email', () => {
+        it('should retrieve engineer email', () => {
+            const email = 'janedoe@gmail.com';
+            const engineer = new Engineer('Jane', 1, email);
+            expect(engineer.getEmail()).toBe(email);
+        });
+    });
+
+    describe('get engineer github', () => {
+        it('should retrieve engineer github', () => {
+            const github = 'janedoe123';
+            const engineer = new Engineer('Jane', 1, 'janedoe@gmail.com', github);
+            expect(engineer.getGithub()).toBe(github);
+        });
+    });
+
+    describe('get engineer role', () => {
+        it('should display engineer as the role', () => {
+            const engineer = new Engineer();
+            expect(engineer.getRole()).toBe('Engineer');
+        });
+    });
+});
